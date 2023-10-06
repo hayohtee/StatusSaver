eec92c7f83d64ea90107981e5844ae382fafb6fe
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-10-06 07:46:57 +0100

    Initial project setup

---
+++ app/src/main/java/dev/hayohtee/statussaver/MainActivity.kt
@@ -0,0 +1,31 @@
+package dev.hayohtee.statussaver
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import dev.hayohtee.statussaver.ui.theme.StatusSaverTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+            StatusSaverTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
