06a03acb6f8e5975d0bff6ae5c8eb08c8c75e1bd
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-10-19 19:55:52 +0100

    Create StatusList.kt

---
+++ app/src/main/java/dev/hayohtee/statussaver/ui/component/StatusList.kt
@@ -0,0 +1,33 @@
+package dev.hayohtee.statussaver.ui.component
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.hayohtee.statussaver.data.Status
+
+@Composable
+fun StatusList(
+    statuses: List<Status>,
+    onSaveStatusClick: (Status) -> Unit,
+    onStatusClick: (Status) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(
+        modifier = modifier.fillMaxSize(),
+        columns = GridCells.Fixed(3)
+    ) {
+        items(items = statuses, key = { it -> it.id }) { status ->
+            StatusItem(
+                status = status,
+                onSaveStatusClick = { onSaveStatusClick(status) },
+                onStatusClick = { onStatusClick(status) },
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
