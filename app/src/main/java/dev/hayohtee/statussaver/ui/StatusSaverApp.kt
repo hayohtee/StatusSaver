4485741e9dc824e7ce67ba64ec87d7e81627196a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-10-19 19:59:43 +0100

    Create StatusSaverApp.kt

---
+++ app/src/main/java/dev/hayohtee/statussaver/ui/StatusSaverApp.kt
@@ -0,0 +1,39 @@
+package dev.hayohtee.statussaver.ui
+
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.hayohtee.statussaver.StatusDocumentContract
+import dev.hayohtee.statussaver.ui.screen.StatusViewModel
+import dev.hayohtee.statussaver.ui.screen.HomeScreen
+
+@Composable
+fun StatusSaverApp(modifier: Modifier = Modifier) {
+    val viewModel = viewModel<StatusViewModel>(factory = StatusViewModel.FACTORY)
+    val uiState = viewModel.uiState
+    val context = LocalContext.current
+    val directoryLauncher = rememberLauncherForActivityResult(
+        contract = StatusDocumentContract(),
+    ) { uri ->
+        if (uri != null) {
+            context.contentResolver.takePersistableUriPermission(
+                uri, Intent.FLAG_GRANT_READ_URI_PERMISSION
+            )
+            viewModel.saveStatusDirectoryUri(uri)
+        }
+    }
+
+    HomeScreen(
+        uiState = uiState,
+        onAccessDirectoryClick = {
+            println("Clicked")
+            directoryLauncher.launch(null)
+        },
+        modifier = modifier
+    )
+}
+
+
