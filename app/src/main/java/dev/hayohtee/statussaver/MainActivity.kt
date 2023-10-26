c7ad73adf4787665e647b9e97f50375f73f081cf
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-10-26 18:53:22 +0100

    Enable splashscreen in MainActivity.kt

--- app/src/main/java/dev/hayohtee/statussaver/MainActivity.kt
+++ app/src/main/java/dev/hayohtee/statussaver/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import dev.hayohtee.statussaver.ui.StatusSaverApp
 import dev.hayohtee.statussaver.ui.theme.StatusSaverTheme
@@ -16,6 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        installSplashScreen()
 
         setContent {
             StatusSaverTheme {
