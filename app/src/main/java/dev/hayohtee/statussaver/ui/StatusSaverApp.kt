d96625ec6d2adfcb8763922bba5ce03a32fc329e
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-10-31 19:05:08 +0100

    Setup navigation in StatusSaverApp.kt

--- app/src/main/java/dev/hayohtee/statussaver/ui/StatusSaverApp.kt
+++ app/src/main/java/dev/hayohtee/statussaver/ui/StatusSaverApp.kt
@@ -1,14 +1,22 @@
 package dev.hayohtee.statussaver.ui
 
 import android.content.Intent
+import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import dev.hayohtee.statussaver.StatusDocumentContract
 import dev.hayohtee.statussaver.ui.screen.HomeScreen
 import dev.hayohtee.statussaver.ui.screen.StatusViewModel
+import dev.hayohtee.statussaver.ui.screen.StatusViewerScreen
 
 @Composable
 fun StatusSaverApp(modifier: Modifier = Modifier) {
@@ -26,15 +34,38 @@ fun StatusSaverApp(modifier: Modifier = Modifier) {
         }
     }
 
-    HomeScreen(
-        uiState = uiState,
-        onAccessDirectoryClick = { directoryLauncher.launch(null) },
-        updateSavedStatus = viewModel::updateSavedStatus,
-        onSaveStatusClick = viewModel::saveStatus,
-        fetchRecentStatuses = viewModel::fetchRecentStatuses,
-        fetchSavedStatuses = viewModel::fetchSavedStatuses,
-        modifier = modifier
-    )
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable(route = "home") {
+            HomeScreen(
+                uiState = uiState,
+                onAccessDirectoryClick = { directoryLauncher.launch(null) },
+                updateSavedStatus = viewModel::updateSavedStatus,
+                onSaveStatusClick = viewModel::saveStatus,
+                fetchRecentStatuses = viewModel::fetchRecentStatuses,
+                fetchSavedStatuses = viewModel::fetchSavedStatuses,
+                onStatusClick = { status ->
+                    navController.navigate("view/${Uri.encode(status.uri.toString())}")
+                },
+                modifier = modifier
+            )
+        }
+
+        composable(
+            route = "view/{uri}",
+            arguments = listOf(navArgument("uri") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val uri = Uri.parse(backStackEntry.arguments?.getString("uri"))
+            StatusViewerScreen(
+                statusUri = uri,
+                navigateBack = { navController.popBackStack() },
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+
+
 }
 
 
