58735d790ca4acd39caf3a4a0b68fe06138516fb
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-11-01 13:02:03 +0100

    Add StatusVideo composable to StatusViewerScreen.kt

--- app/src/main/java/dev/hayohtee/statussaver/ui/screen/StatusViewerScreen.kt
+++ app/src/main/java/dev/hayohtee/statussaver/ui/screen/StatusViewerScreen.kt
@@ -1,6 +1,8 @@
 package dev.hayohtee.statussaver.ui.screen
 
 import android.net.Uri
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.FrameLayout
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,12 +16,20 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.C
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import coil.compose.rememberAsyncImagePainter
 import dev.hayohtee.statussaver.R
 import dev.hayohtee.statussaver.ui.theme.StatusSaverTheme
@@ -71,6 +81,11 @@ fun StatusViewerScreen(
                     statusName = name,
                     modifier = Modifier.matchParentSize()
                 )
+            } else {
+                StatusVideo(
+                    statusUri = statusUri,
+                    modifier = Modifier.matchParentSize()
+                )
             }
         }
     }
@@ -85,12 +100,41 @@ fun StatusImage(statusUri: Uri, statusName: String, modifier: Modifier = Modifie
     )
 }
 
+@Composable
+@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
+fun StatusVideo(statusUri: Uri, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context).build().apply {
+            setMediaItem(MediaItem.fromUri(statusUri))
+            prepare()
+        }
+    }
+    exoPlayer.playWhenReady = true
+    exoPlayer.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT
+    exoPlayer.repeatMode = Player.REPEAT_MODE_ONE
+
+    DisposableEffect(Unit) {
+        onDispose { exoPlayer.release() }
+    }
+
+    AndroidView(
+        factory = {
+            PlayerView(context).apply {
+                player = exoPlayer
+                layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+            }
+        },
+        modifier = modifier
+    )
+}
+
 @Preview
 @Composable
 fun StatusViewerScreenPreview() {
     StatusSaverTheme {
         StatusViewerScreen(
-            statusUri = Uri.parse("https://www.google.com"),
+            statusUri = Uri.parse(""),
             navigateBack = {}
         )
     }
